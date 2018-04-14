9ac9327ef7d67d8d712597fb83c28fa6bb9adbe4
Jonathan Murphy <jonathan@jmurphy.me>
2018-04-14 19:52:23 -0400

    init

---
+++ classes.py
@@ -0,0 +1,52 @@
+class Variable_Holder:
+    Command_Array = []
+    Sleep_Time = 1000
+    """
+    Sleep_Time in ms
+    """
+    Headlight_Methods = ["On", "Off", "Hazards", "Blink_On", "Blink_Off"]
+    Headlight_Current = 0
+
+    Status_Lights = {
+        "Initial": ("Blink", "Yellow"),
+        "Error": ("Solid", "Red"),
+        "Disconnected": ("Blink", "Blue"),
+        "Good": ("Solid", "Green")}
+    Status_Current = 0
+
+    last_pop = (0,0)
+
+    def append_to_array(self, data):
+        if isinstance(data[0], (int, float)) and isinstance(data[1], (int, float)):
+            self.Command_Array.append((data[0], data[1]))
+
+    def pop_array(self):
+        try:
+            data = self.Command_Array.pop(0)
+            self.last_pop = data
+            return data
+        except:
+            return (self.last_pop[0], 0)
+
+    def get_status(self):
+        return self.Status_Lights[self.Status_Current]
+
+    def change_status(self, num):
+        if isinstance(num, int):
+            if (0 <= num) and (num <= (len(self.Status_Lights) +1)):
+                self.Status_Current = num
+
+    def get_headlights(self):
+        return self.Headlight_Methods[self.Headlight_Current]
+
+    def change_headlights(self, num):
+        if isinstance(num, int):
+            if (0 <= num) and (num <= (len(self.Headlight_Methods) +1)):
+                self.Headlight_Current = num
+
+    def get_sleep_time(self):
+        return self.Sleep_Time
+
+    def update_sleep_time(self, num):
+        if isinstance(num, int):
+            self.Sleep_Time = num
---
+++ main.py
@@ -0,0 +1,69 @@
+import Settings
+import time
+from classes import Variable_Holder as VH
+import pigpio
+import math
+import asyncio
+import websockets
+import json
+import threading
+
+def set_servo(num):
+    cos_input = math.cos(math.radians(num))
+	servo_input = 1500 + (cos_input * 950)
+    pi.set_servo_pulsewidth(Settings.Steering_Pin, servo_input)
+
+def set_motor(servo, num):
+    mid = (Settings.PWM_High + Settings.PWM_Low)/2
+    mid_scale = Settings.PWM_High - mid
+    scale = (num/100)*mid_scale
+    if (math.sin(math.radians(servo)) < 0):
+        scale = scale * -1
+    pi.set_servo_pulsewidth(Settings.ESC_Pin, scale + mid)
+
+def connect_to_ws():
+    async def connect():
+        str = 'ws://' + Settings.domain + '/ws/' + Settings.Server_UUID + '/data/'
+        data = {}
+        data['type'] = "Login"
+        data['password'] = Settings.Server_Password
+
+        async with websockets.connect(str) as websocket:
+            sleep(1)
+            await websocket.send(json.dumps(data))
+            while True:
+                data = await websockets.recv()
+                data = json.loads(data)
+                try:
+                    drive = data['drive']
+                    scale = data['scale']
+                except KeyError:
+                    print("KeyError, should ignore\n")
+                vars.append_to_array((drive, scale))
+    asyncio.get_event_loop().run_until_complete(connect())
+
+def do_servos():
+    while True:
+        data = VH.pop_array()
+        set_servo(data[0])
+        set_motor(data[0], data[1])
+        time.sleep(VH.get_sleep_time()/1000)
+
+def init_servo():
+    pi.set_servo_pulsewidth(Settings.ESC_Pin, setting.PWM_High)
+    time.sleep(1)
+    pi.set_servo_pulsewidth(Settings.ESC_Pin, setting.PWM_Low)
+    time.sleep(1)
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    init_servo()
+    global vars
+    vars = VH()
+    global pi
+    pi = pigpio.pi()
+    servo_thread = threading.Thread(target=do_servos)
+    servo_thread = threading.Thread(target=connect_to_ws)
+
+if __name__ == "__main__":
+    main()
